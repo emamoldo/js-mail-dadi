2b4233b6a1efc8d65373dfe9bb821f2f11be5b75
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-22 17:34:45 +0100

    Add Comments in JS

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/* devo creare un ciclo che mi permette di avere un numero random da 1 a 6
+    - devo spampare in console il numero:
+        - per il giocatore;
+        - per il computer;
+
+    - devo comparare i due numeri per vedere qual'è il più grande:
+        - stampare quel numero; 
+*/
+
+let numbers = [1, 2, 3, 4, 5, 6]
+const elementName = document.getElementById("container")
+
+
+// ``
