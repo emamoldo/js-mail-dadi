cfae28c4b457c20268c15be50db6002006d94d81
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-22 18:06:26 +0100

    Add the structure of the random numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,44 @@
         - stampare quel numero; 
 */
 
-let numbers = [1, 2, 3, 4, 5, 6]
+let numbers = [1, 2, 3, 4, 5, 6];
 const elementName = document.getElementById("container")
 
-// Creating a cicle with random numbers
 
-for (let i = 0; i < 2; i++) {
-    let count = 1;
+let player_score =  Math.floor(Math.random() * 6) + 1,
+computer_score =  Math.floor(Math.random() * 6) + 1;
+
+if (player_score == computer_score) {
+    elementName.insertAdjacentHTML('beforeend', `<p>${player_score} I numeri sono uguali, Parità</p>`)
+    
+} else if (computer_score > player_score) {
+    elementName.insertAdjacentHTML('beforeend', `<p>${computer_score} Il computer ha vinto</p>`)
+
+} else{
+    elementName.insertAdjacentHTML('beforeend', `<p>${player_score} Il giocatire ha vinto</p>`)
+
+}
+
+
+
+
+
+
+
+
+
+/* Creating a cicle with random numbers
+
+for (let i = 0; i < 1; i++) {
+
+     let count = 1;
     for (numbers = 0; numbers < Math.floor(Math.random() * 6); numbers++) {
         count++;
     }
     console.log(count);
-    /*elementName.insertAdjacentHTML('beforeend', `<p>${count}</p>`)*/
+
+    elementName.insertAdjacentHTML('beforeend', `<p>${count}</p>`)
+    elementName.innerHTML="";
     
     if (count === numbers) {
         elementName.insertAdjacentHTML('beforeend', `<p>${count} I numeri sono uguali, Parità</p>`)
@@ -29,8 +55,7 @@ for (let i = 0; i < 2; i++) {
     } else{
         elementName.insertAdjacentHTML('beforeend', `<p>${count} Il giocatire ha vinto</p>`)
 
-    }
-
-}
+    } 
+}*/
 
 // ``
