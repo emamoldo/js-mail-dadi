4413f160ad864283af8e02a2c96b600ff8d56aa9
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-22 17:35:07 +0100

    Add the cicle for the random numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,5 +10,27 @@
 let numbers = [1, 2, 3, 4, 5, 6]
 const elementName = document.getElementById("container")
 
+// Creating a cicle with random numbers
+
+for (let i = 0; i < 2; i++) {
+    let count = 1;
+    for (numbers = 0; numbers < Math.floor(Math.random() * 6); numbers++) {
+        count++;
+    }
+    console.log(count);
+    /*elementName.insertAdjacentHTML('beforeend', `<p>${count}</p>`)*/
+    
+    if (count === numbers) {
+        elementName.insertAdjacentHTML('beforeend', `<p>${count} I numeri sono uguali, Parità</p>`)
+        
+    } else if (count > numbers) {
+        elementName.insertAdjacentHTML('beforeend', `<p>${count} Il computer ha vinto</p>`)
+
+    } else{
+        elementName.insertAdjacentHTML('beforeend', `<p>${count} Il giocatire ha vinto</p>`)
+
+    }
+
+}
 
 // ``
